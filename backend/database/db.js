9a07cf7f106e39a7fa425f92be3bfb8b97e47f2c
raissadurau <raissadurau22@gmail.com>
2025-06-16 11:37:50 -0300

    asdsdasda

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,8 +1,17 @@
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "backend/database/tarefas.sqlite"
+// const sequelize = new Sequelize({
+//   dialect: "sqlite",
+//   storage: "backend/database/tarefas.sqlite"
+// });
+
+// module.exports = sequelize;
+const {Sequelize} = require("sequelize");
+
+const  sequelize = new Sequelize("cadastro_tarefas", "tarefas_user", "123456",{
+  host: "localhost", 
+  dialect: "mysql",
+  timestamp: false,
 });
 
 module.exports = sequelize;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,42 @@
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const tarefaRoutes = require("./routes/tarefaRoutes");
+// const sequelize = require("./database/db");
+// const Tarefa = require("./models/Tarefa");
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(tarefaRoutes);
+
+// sequelize.sync().then(() => {
+//   app.listen(3000, () => {
+//     console.log("Servidor rodando em http://localhost:3000");
+//   });
+// });
+
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const tarefaRoutes = require("./routes/tarefaRoutes");
-const sequelize = require("./database/db");
-const Tarefa = require("./models/Tarefa");
+const sequelize = ("./database/db");
+const app = express(); 
 
+//MIDLEWARE
 app.use(cors());
 app.use(express.json());
-app.use(tarefaRoutes);
 
-sequelize.sync().then(() => {
+//ROTAS
+app.use("/", tarefaRoutes);
+
+
+//SINCRONIZAÇÃO COM O BANCO - INICIAR DO SERVIDOR
+sequelize.sync()
+.then(() => {
+  console.log("Banco de dados sincronizado com sucesso");
   app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000");
+    console.log("Servidor rodando em http://localhost:3000")
   });
+})
+.catch((error)=> {
+  console.error("Erro ao conectar ao banco de dados: ", error);
 });
